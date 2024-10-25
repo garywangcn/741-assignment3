0653053fc80e03a323301a9fe295a410393027df
Gary Wang <garywangcn@outlook.com>
2024-10-25 21:20:03 +0000

    update calc_next_plain

--- calc_next_plain.py
+++ calc_next_plain.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 from Crypto.Util.Padding import pad
 from binascii import unhexlify, hexlify
 
@@ -9,17 +11,14 @@ block_size = 16
 padded_message = pad(message.encode(), block_size)
 padded_hex = hexlify(padded_message)
 
-# Convert hex strings to bytes
 padded_message_bytes = unhexlify(padded_hex)
 hex1_bytes = unhexlify(IV)
 hex2_bytes = unhexlify(nextIV)
 
-# Step 3: XOR the values
 result = bytearray()
 for i in range(len(padded_message_bytes)):
     result.append(padded_message_bytes[i] ^ hex1_bytes[i] ^ hex2_bytes[i])
 
-# Step 4: Print the result as hex
 result_hex = hexlify(result).decode()
 print("Adjusted Plaintext P' :", result_hex)
 
